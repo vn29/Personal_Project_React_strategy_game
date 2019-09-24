aeebf9e8c358966ef8a47ced704eb6f0ceb05fa9
Evan Prado <41770458+vn29@users.noreply.github.com>
2019-09-23 23:47:47 -0700

    condensed logic to master component

--- components/ResourceImage.js
+++ components/ResourceImage.js
@@ -4,15 +4,9 @@ import ReactDOM from 'react-dom';
 class ResourceImage extends React.Component {
 
      render(){
-          if (this.props.rname == "factories") {
-               var rname = "1f";
-          };
-          if (this.props.rname == "workers") {
-               var rname = "1w"
-          }
           return (
                <div  className = "RImage">
-                    <p>{rname} </p>
+                    <p>{this.props.rname} </p>
                </div>
           );
      }
