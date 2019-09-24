e3a93d577b73e65a75290f07ec94b1b78db4936d
Evan Prado <41770458+vn29@users.noreply.github.com>
2019-09-23 20:26:05 -0700

    now resource image shows if a factory or a worker

--- components/ResourceImage.js
+++ components/ResourceImage.js
@@ -4,9 +4,15 @@ import ReactDOM from 'react-dom';
 class ResourceImage extends React.Component {
 
      render(){
+          if (this.props.rname == "factories") {
+               var rname = "1f";
+          };
+          if (this.props.rname == "workers") {
+               var rname = "1w"
+          }
           return (
-               <div>
-                    <p>1f</p>
+               <div  className = "RImage">
+                    <p>{rname} </p>
                </div>
           );
      }
