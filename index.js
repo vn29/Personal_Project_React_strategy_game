24d43d46b44a3d7e4fb43a6956cfbbb0dee07be0
Evan Prado <41770458+vn29@users.noreply.github.com>
2019-09-23 23:46:36 -0700

    condensed logic to master component

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import './components/ResourceDisplay.js';
 import ResourceDisplay from './components/ResourceDisplay.js';
+import ResourceImage from './components/ResourceImage.js';
 
 
    
@@ -10,13 +11,13 @@ import ResourceDisplay from './components/ResourceDisplay.js';
         constructor(props){
              super(props);
              this.state = {
-               energy : 1,
-               minerals : 1,
-               food : 1,
-               alloys : 1,
-               factories : 1,
-               workers : 1,
-               day : 1,
+               energy : 1.,
+               minerals : 1.,
+               food : 1.,
+               alloys : 1.,
+               factories : 1.,
+               workers : 1.,
+               day : 1.,
                message : "",
              };
         }
@@ -32,7 +33,7 @@ import ResourceDisplay from './components/ResourceDisplay.js';
 
           var new_energy = energy + 1.*factories/2*workers/4 - workers/16;
           var new_minerals = minerals + 1.*factories*workers;
-          var new_food = food + 1.*factories/4*workers/4 - workers/8;
+          var new_food = food + 1.*factories/2*workers/2 - workers/8;
           var new_alloys = alloys + 1.*factories/3*workers/4*minerals/4;
           var new_factories = factories;
           var new_workers = workers;
@@ -59,13 +60,30 @@ import ResourceDisplay from './components/ResourceDisplay.js';
           } else {
                this.setState({
                     factories : t.factories + 1,
-                    energy : t.energy -1,
-                    minerals : t.minerals -4,
-                    workers : t.workers -1,
-                    alloys : t.allys -0.5,
+                    energy : t.energy - 1,
+                    minerals : t.minerals - 4,
+                    workers : t.workers - 1,
+                    alloys : t.alloys - 0.5,
                })
           }
      }
+
+     rdcf() {
+          const fact_range = [...Array(this.state.factories).keys()];
+          const factory_icons = fact_range.map((factory) =>
+               <ResourceImage rname = {"f" + (factory + 1)}/>
+          );
+          return factory_icons;
+     }
+
+     rdcw() {
+          const worker_range = [...Array(this.state.workers).keys()];
+          const worker_icons = worker_range.map((worker) =>
+               <ResourceImage rname = {"w"+ (worker + 1)} />
+          );
+          return worker_icons;
+     }
+
      hire_worker() {
           var t = this.state;
           if (t.energy <0.5) {
@@ -85,14 +103,14 @@ import ResourceDisplay from './components/ResourceDisplay.js';
                <button onClick = {() => this.build_factory()} >Build Factory</button>
 
                <div className = "display">
-                    <ResourceDisplay resourceName = {"message"} passedvalue1 = {this.state.message} passedvalue2 = {this.state}/>
-                    <ResourceDisplay resourceName = {"energy"} passedvalue1 = {this.state.energy} passedvalue2 = {this.state}/>
-                    <ResourceDisplay resourceName = {"minerals"} passedvalue1 = {this.state.minerals} passedvalue2 = {this.state}/>
-                    <ResourceDisplay resourceName = {"food"} passedvalue1 = {this.state.food} passedvalue2 = {this.state}/>
-                    <ResourceDisplay resourceName = {"alloys"} passedvalue1 = {this.state.alloys} passedvalue2 = {this.state}/>
-                    <ResourceDisplay resourceName = {"factories"} passedvalue1 = {this.state.factories} passedvalue2 = {this.state}/>
-                    <ResourceDisplay resourceName = {"workers"} passedvalue1 = {this.state.workers} passedvalue2 = {this.state}/>
-                    <ResourceDisplay resourceName = {"day"} passedvalue1 = {this.state.day} passedvalue2 = {this.state}/>
+                    <ResourceDisplay resourceName = {"message"} passedvalue1 = {this.state.message}/>
+                    <ResourceDisplay resourceName = {"energy"} passedvalue1 = {this.state.energy}/>
+                    <ResourceDisplay resourceName = {"minerals"} passedvalue1 = {this.state.minerals}/>
+                    <ResourceDisplay resourceName = {"food"} passedvalue1 = {this.state.food}/>
+                    <ResourceDisplay resourceName = {"alloys"} passedvalue1 = {this.state.alloys}/>
+                    <ResourceDisplay resourceName = {"factories"} passedMethod = {() => this.rdcf()}/>
+                    <ResourceDisplay resourceName = {"workers"} passedMethod = {() => this.rdcw()}/>
+                    <ResourceDisplay resourceName = {"day"} passedvalue1 = {this.state.day}/>
                     
                </div>
             </div>
