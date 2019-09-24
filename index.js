b8fe2eb54290e66ce00e38f81a6398fd2775ef2a
Evan Prado <41770458+vn29@users.noreply.github.com>
2019-09-23 23:55:03 -0700

    correction to naming

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ import ResourceImage from './components/ResourceImage.js';
 
 
    
-   class Master extends React.Component {
+   class Game extends React.Component {
         constructor(props){
              super(props);
              this.state = {
@@ -121,7 +121,7 @@ import ResourceImage from './components/ResourceImage.js';
    // ========================================
    
    ReactDOM.render(
-     <Master />,
+     <Game />,
      document.getElementById('root')
    );
    
