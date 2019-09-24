b28daea7f1cd7e494a1e1f74593d91a00a800716
Evan Prado <41770458+vn29@users.noreply.github.com>
2019-09-23 19:56:12 -0700

    Create ResourceImage.js

---
+++ components/ResourceImage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class ResourceImage extends React.Component {
+
+     render(){
+          return (
+               <div>
+                    <p>1f</p>
+               </div>
+          );
+     }
+}
+
+export default ResourceImage;
