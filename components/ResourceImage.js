8c47a56027922d897d2d06b6bd1f5112a77efb97
Evan Prado <41770458+vn29@users.noreply.github.com>
2019-09-24 19:50:25 -0700

    png icons now included

--- components/ResourceImage.js
+++ components/ResourceImage.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+
 class ResourceImage extends React.Component {
 
      render(){
           return (
                <div  className = "RImage">
-                    <p>{this.props.rname} </p>
+                    {/* <p>{this.props.rname} </p>  */}
+                    <img src={this.props.imglink} height='10px' width='10px' display='inline-block'/>
                </div>
           );
      }
