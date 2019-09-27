b8098795d5df30bb61376e63abd94af664a57da8
Evan Prado <41770458+vn29@users.noreply.github.com>
2019-09-26 23:21:26 -0700

    Create Squares.js

---
+++ components/Squares.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import NumberFormat from 'react-number-format';
+import Button from 'react-bootstrap/Button';
+import ButtonGroup  from 'react-bootstrap/ButtonGroup';
+
+
+class Square extends React.Component {
+
+     render(){
+          return(<div className = "square">
+                    
+                    <ButtonGroup className = "mr_inline">
+                              <Button variant="secondary" 
+                              size="sm" 
+                              onClick = {this.props.fx[0]} >+</Button>
+                              <Button variant="secondary" 
+                              size="sm" 
+                              onClick = {this.props.fx[1]} >-</Button>
+                    </ButtonGroup>
+               </div>
+               );
+     }
+}
+
+export default Square;
