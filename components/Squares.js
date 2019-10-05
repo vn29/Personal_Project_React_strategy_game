94a7534a3c65860155b4d420567f26e91095e8b6
Evan Prado <41770458+vn29@users.noreply.github.com>
2019-10-04 21:33:27 -0700

    Update Squares.js

--- components/Squares.js
+++ components/Squares.js
@@ -1,27 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import NumberFormat from 'react-number-format';
-import Button from 'react-bootstrap/Button';
-import ButtonGroup  from 'react-bootstrap/ButtonGroup';
-
+import React from "react";
+import ReactDOM from "react-dom";
+import NumberFormat from "react-number-format";
+import Button from "react-bootstrap/Button";
+import ButtonGroup from "react-bootstrap/ButtonGroup";
 
 class Square extends React.Component {
-
-     render(){
-          return(<div className = "square">
-                    {/* {this.props.idd} */}
-                    {this.props.num_factories + ' '}
-                    <ButtonGroup className = "mr_inline">
-                              <Button variant="secondary" 
-                              size="sm" 
-                              onClick = {this.props.fx[0]} >+</Button>
-                              <Button variant="secondary" 
-                              size="sm" 
-                              onClick = {this.props.fx[1]} >-</Button>
-                    </ButtonGroup>
-               </div>
-               );
-     }
+  render() {
+    return (
+      <div className="square">
+        <div className = 'teritory-title'>teritory</div>
+        {/* {this.props.idd} */}
+        {this.props.num_factories + " "}
+        <ButtonGroup className="mr_inline">
+          <Button variant="secondary" size="sm" onClick={this.props.fx[0]}>
+            +
+          </Button>
+          <Button variant="secondary" size="sm" onClick={this.props.fx[1]}>
+            -
+          </Button>
+        </ButtonGroup>
+      </div>
+    );
+  }
 }
 
 export default Square;
